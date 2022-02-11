edf5306172089f5e40a6449b657112306d88dc35
manu-devops98 <manuelcappello7@gmail.com>
2022-02-11 15:58:31 +0100

    add header

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,17 +1,30 @@
 <header>
-    <div class="container-80 flex">
-      <img src="{{asset('img/dc-logo.png')}}" alt="" />
-      <ul class="flex">
-        <li><a href="">CHARACTERS</a></li>
-        <li><a class="active" href="">COMICS</a></li>
-        <li><a href="">MOVIES</a></li>
-        <li><a href="">TV</a></li>
-        <li><a href="">GAMES</a></li>
-        <li><a href="">COLLECTIBLES</a></li>
-        <li><a href="">VIDEOS</a></li>
-        <li><a href="">FANS</a></li>
-        <li><a href="">NEWS</a></li>
-        <li><a href="">SHOP</a></li>
-      </ul>
+    <div class="header-top">
+        <div class="container-80 flex">
+            <h4>DC POWER VISA</h4>
+            <h4>ADDITIONAL DC SITES</h4>
+        </div>
+    </div>
+    <div class="container-80">
+        <div class="header-bottom flex">
+            <img src="{{asset('img/dc-logo.png')}}" alt="" />
+            <ul class="flex">
+              <li><a href="">CHARACTERS</a></li>
+              <li><a class="active" href="">COMICS</a></li>
+              <li><a href="">MOVIES</a></li>
+              <li><a href="">TV</a></li>
+              <li><a href="">GAMES</a></li>
+              <li><a href="">COLLECTIBLES</a></li>
+              <li><a href="">VIDEOS</a></li>
+              <li><a href="">FANS</a></li>
+              <li><a href="">NEWS</a></li>
+              <li><a href="">SHOP</a></li>
+            </ul>
+            <div class="input">
+                <form action="">
+                    <input type="text" placeholder="Search">
+                </form>
+            </div>
+        </div>
     </div>
 </header>
