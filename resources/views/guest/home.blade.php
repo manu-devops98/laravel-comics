d45ddeecde52cc8810ea27176218d1a053e41fa7
manu-devops98 <manuelcappello7@gmail.com>
2022-02-11 16:54:42 +0100

    add moidifiche main

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -22,5 +22,32 @@
                 </div>
             @endforeach
         </div>
+        <div class="button flex">
+            <button><a href="">LOAD MORE</a></button>
+        </div>
+        <div class="container-icons">
+            <div class="container-80">
+                <div class="box">
+                    <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+                    <h3>DIGITAL COMICS</h3>
+                </div>
+                <div class="box">
+                    <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="">
+                    <h3>DC MERCHANDISE</h3>
+                </div>
+                <div class="box">
+                    <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="">
+                    <h3>SUBSCRIPTION</h3>
+                </div>
+                <div class="box">
+                    <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="">
+                    <h3>COMIC SHOP LOCATOR</h3>
+                </div>
+                <div class="box">
+                    <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="">
+                    <h3>DC POWER VISA</h3>
+                </div>
+            </div>
+        </div>
     </main>
 @endsection
