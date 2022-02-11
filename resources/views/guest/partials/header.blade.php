68420031bf067729b790be6ee4e56c4a4f95ac91
manu-devops98 <manuelcappello7@gmail.com>
2022-02-11 15:28:32 +0100

    add errore

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,3 +1,17 @@
 <header>
-    <h1>Header</h1>
+    <div class="container-80 flex">
+      <img src="{{asset('img/dc-logo.png')}}" alt="" />
+      <ul class="flex">
+        <li><a href="">CHARACTERS</a></li>
+        <li><a class="active" href="">COMICS</a></li>
+        <li><a href="">MOVIES</a></li>
+        <li><a href="">TV</a></li>
+        <li><a href="">GAMES</a></li>
+        <li><a href="">COLLECTIBLES</a></li>
+        <li><a href="">VIDEOS</a></li>
+        <li><a href="">FANS</a></li>
+        <li><a href="">NEWS</a></li>
+        <li><a href="">SHOP</a></li>
+      </ul>
+    </div>
 </header>
